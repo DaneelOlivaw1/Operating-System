48c59e2dbe2a88da36bedbd6528ade2be1589f76
DaneelOlivaw1 <2265970279@qq.com>
2020-11-06 12:10:06 +0800

    master

--- SourceCode/src/OS.h
+++ SourceCode/src/OS.h
@@ -5,8 +5,8 @@ class OS
 private:
     static const int MAXN = 10000;
 public:
-    int memory[MAXN];
-    bool IO[MAXN];
+    int memory[MAXN]; // 存储程序占用时间
+    bool Source[MAXN]; // 是否使用竞争资源 0为不使用 1为使用
     int p;
 public:
     OS(/* args */);
--- SourceCode/src/usr.cpp
+++ SourceCode/src/usr.cpp
@@ -8,12 +8,22 @@ usr::usr(string name)
 }
 
 bool usr::Write(OS &os){
+    // 在操作系统内存中写入程序
+    // 程序长度为length为20～50
+    // 占用时间为10～30
+    // 占用竞争资源的概率为30% 抢占CPU的概率为70%
+    int length = GetRandomNum(20, 51);
+    for (int i = 1; i <= length; i++){
+        os.memory[os.p] = GetRandomNum(10, 30);
+        GetRandomNum(1,11) <= 3 ? os.Source[os.p] = 1 : os.Source[os.p] = 0;
+        os.p++;
+    }
     return true;
 }
 
 int usr::GetRandomNum(int l, int r){
-    //unsigned seed = time();
-    //srand(seed);
+    // 生成随机数
+    // 随机数范围 [l,r)
     int res = (rand() % (r-l))+ l;
     return res;
 }
