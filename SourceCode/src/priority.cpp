977f9e2c253cbe01bfcbee3d2ebfa7fb352fcd6c
13407797515 <yuanfei496@gmail.com>
2020-11-09 20:50:33 +0800

    master

---
+++ SourceCode/src/priority.cpp
@@ -0,0 +1,22 @@
+#include"priority.h"
+#include "stdio.h"
+void priority::FeedIn(OS os){            //指向当前优先级最高进程的指针，level越小优先级越大
+    int size = os.Ready_q.size();
+    for(int i = 0; i < size;i++)
+    {
+        auto A = os.Ready_q.front();
+        os.Ready_q.pop();
+        Ready.push(A);
+    }
+
+}
+// void priority::print(){
+//     while(Ready.size() != NULL)
+//     {
+//         auto p = Ready.top();
+//         Ready.pop();
+//         cout << "正在运行的进程" << endl;
+// 		cout << "进程名\t优先数 服务时间 已运行时间 还剩运行时间" << endl;//输出当前进程
+
+//     }
+// }
--- SourceCode/src/process.h
+++ SourceCode/src/process.h
@@ -14,6 +14,15 @@ public:
     int cnt; //当前运行了多少行
     int arrivetime; // 到达时间。
 public:
+    int FinishTime;     //结束时间
+    int TurnTime;       //周转时间
+    int DaiquanTime;  //带权周转时间
+    int ServerTime;  //服务时间
+    bool operator<(const struct process &a)const {              //优先权排
+		return level > a.level || level == a.level;
+	}
+
+public:
     process(int start, int length, string user); //start: 开始地址, length: 代码块长度, user: 用户名
     ~process();
     bool operator<(const process& rhs);
